db4172dd8199ad6858cf151ce7d71300ffd0d8a0
Hubert Kowalski <hubert160491@gmail.com>
2021-05-26 23:03:48 +0200

    Stworzenie projektu

---
+++ src/main/java/pl/hk/zadanie_29/Zadanie29Application.java
@@ -0,0 +1,13 @@
+package pl.hk.zadanie_29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Zadanie29Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Zadanie29Application.class, args);
+    }
+
+}
---
+++ src/test/java/pl/hk/zadanie_29/Zadanie29ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hk.zadanie_29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Zadanie29ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
